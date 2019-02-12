c9301019f77c95d58f74c94d7cf7ad94e16ebfe8
alex <alex@tekartik.com>
2019-02-12 22:28:05 +0100

    fix: lint rules

--- test/cmd_record_test.dart
+++ test/cmd_record_test.dart
@@ -1,11 +1,12 @@
 @TestOn("vm")
 import 'dart:convert';
-import 'package:path/path.dart';
-import 'package:tekartik_cmd_record/cmd_record.dart';
-import 'package:dev_test/test.dart';
-import 'package:process_run/cmd_run.dart';
-import 'package:tekartik_common_utils/common_utils_import.dart';
 import 'dart:io';
+
+import 'package:dev_test/test.dart';
+import 'package:path/path.dart';
+import 'package:process_run/cmd_run.dart';
+import 'package:tekartik_cmd_record/src/cmd_record.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
 
 String get cmdRecordScriptPath => join('bin', 'cmd_record.dart');
 
@@ -15,14 +16,14 @@ void main() {
   group('cmd_record', () {
     test('version', () async {
       ProcessResult result =
-          await runCmd(dartCmd([cmdRecordScriptPath, '--version']));
+          await runCmd(DartCmd([cmdRecordScriptPath, '--version']));
       expect(result.stderr.toLowerCase(), contains("version"));
       // "Dart VM version: 1.7.0-dev.4.5 (Thu Oct  9 01:44:31 2014) on "linux_x64"\n"
     });
 
     test('out', () async {
-      History history = new History();
-      ProcessCmd cmd = dartCmd([echoScriptPath, '--stdout', 'out']);
+      History history = History();
+      ProcessCmd cmd = DartCmd([echoScriptPath, '--stdout', 'out']);
       await record(cmd.executable, cmd.arguments,
           noStdOutput: true, history: history);
       //devPrint(JSON.encode(history));
@@ -30,22 +31,22 @@ void main() {
     });
 
     test('err', () async {
-      History history = new History();
-      ProcessCmd cmd = dartCmd([echoScriptPath, '--stderr', 'err']);
+      History history = History();
+      ProcessCmd cmd = DartCmd([echoScriptPath, '--stderr', 'err']);
       await record(cmd.executable, cmd.arguments,
           noStdOutput: true, history: history);
       //devPrint(JSON.encode(history));
       expect(history.errItems.first.line, "err");
-    }, onPlatform: {"windows": new Skip("failing")});
+    }, onPlatform: {"windows": const Skip("failing")});
 
     test('in', () async {
-      History history = new History();
-      ProcessCmd cmd = dartCmd([echoScriptPath, '--stdin']);
-      var inController = new StreamController<List<int>>();
+      History history = History();
+      ProcessCmd cmd = DartCmd([echoScriptPath, '--stdin']);
+      var inController = StreamController<List<int>>();
       Future future = record(cmd.executable, cmd.arguments,
           noStdOutput: true, inStream: inController.stream, history: history);
       inController.add(utf8.encode("in"));
-      inController.close();
+      await inController.close();
       await future;
       //devPrint(JSON.encode(history));
       expect(history.inItems.first.line, "in");
--- test/src_utils_test.dart
+++ test/src_utils_test.dart
@@ -4,10 +4,10 @@ import 'package:dev_test/test.dart';
 void main() {
   group('src_utils', () {
     test('durationToString', () {
-      expect(durationToString(new Duration(milliseconds: 900)), "00:00.900");
-      expect(durationToString(new Duration(milliseconds: 90)), "00:00.090");
-      expect(durationToString(new Duration(milliseconds: 61090)), "01:01.090");
-      expect(durationToString(new Duration()), "00:00.000");
+      expect(durationToString(Duration(milliseconds: 900)), "00:00.900");
+      expect(durationToString(Duration(milliseconds: 90)), "00:00.090");
+      expect(durationToString(Duration(milliseconds: 61090)), "01:01.090");
+      expect(durationToString(Duration()), "00:00.000");
     });
   });
 }
