6bef2f27a54be7253e3f52ee9bf5c46ac59c8c39
alex <alex@tekartik.com>
2019-09-11 08:09:14 +0200

    build: fix lint

--- test/src_utils_test.dart
+++ test/src_utils_test.dart
@@ -4,10 +4,11 @@ import 'package:dev_test/test.dart';
 void main() {
   group('src_utils', () {
     test('durationToString', () {
-      expect(durationToString(Duration(milliseconds: 900)), "00:00.900");
-      expect(durationToString(Duration(milliseconds: 90)), "00:00.090");
-      expect(durationToString(Duration(milliseconds: 61090)), "01:01.090");
-      expect(durationToString(Duration()), "00:00.000");
+      expect(durationToString(const Duration(milliseconds: 900)), "00:00.900");
+      expect(durationToString(const Duration(milliseconds: 90)), "00:00.090");
+      expect(
+          durationToString(const Duration(milliseconds: 61090)), "01:01.090");
+      expect(durationToString(const Duration()), "00:00.000");
     });
   });
 }
