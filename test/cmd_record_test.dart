217392a0154cd9677556c62a36519dd8b0d49f19
alex <alex@tekartik.com>
2021-04-14 08:14:27 +0200

    fix travis

--- test/cmd_record_test.dart
+++ test/cmd_record_test.dart
@@ -15,7 +15,7 @@ void main() {
   group('cmd_record', () {
     test('version', () async {
       final result = await runCmd(DartCmd([cmdRecordScriptPath, '--version']));
-      expect(result.stderr.toLowerCase(), contains('version'));
+      expect(result.stderr.toString().toLowerCase(), contains('version'));
       // 'Dart VM version: 1.7.0-dev.4.5 (Thu Oct  9 01:44:31 2014) on 'linux_x64'\n'
     });
 
